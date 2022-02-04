7ab9023732a259c47bffeb14b2a3c912e0730006
Ryan Ashby <rjapsu160@gmail.com>
2022-02-04 15:50:13 -0800

    add styling and experimenting with video webpack plugins

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import Astronaut from "./astronaut";
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    // $("#negative").text("");
     let age = parseInt($("input#age").val());
     let planet = $("select#planet option:selected").val();
     let astronaut = new Astronaut(age, planet);
     window.scrollTo(0,document.body.scrollHeight);
-    astronaut.whereFrom();
-    astronaut.howMuchLeft();
     $("#outPlanet").text(planet);
     $("#outAge").html(astronaut.whereFrom());
     $("#leftPos").html(astronaut.howMuchLeft());
-    return astronaut;
+    $("#leftNeg").html(astronaut.outlive());
+    $(".results").fadeIn();
   });
 });
