d812501db17874623efa7de2726a0f47a8cb1b48
Ryan Ashby <rjapsu160@gmail.com>
2022-02-04 12:02:57 -0800

    write test for outliving life expectancy/break for lunch

--- __tests__/convertAge.test.js
+++ __tests__/convertAge.test.js
@@ -80,4 +80,10 @@ describe("Astronaut", () => {
     expect(astronaut.planet).toEqual("");
     expect(astronaut.howMuchLeft()).toEqual("Choose a Planet");
   });
+
+  test("should return the message 'Congratulations! You've out-kicked your coverage age by' plus a positive number of years if a user has outlived their life expectancy", () => {
+    const astronaut = new Astronaut(83, "Earth"); 
+    expect(astronaut.planet).toEqual("Earth");
+    expect(astronaut.howMuchLeft()).toEqual("Congratulations! You've out-kicked your coverage age by X years!");
+  });
 })
