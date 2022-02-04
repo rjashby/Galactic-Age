c7e8e0af8b4382b067d7ca0b3f497b184070a867
Ryan Ashby <rjapsu160@gmail.com>
2022-02-04 10:18:09 -0800

    add parseFloat and toFixed methods for age/pass Mercury test

--- __tests__/convertAge.test.js
+++ __tests__/convertAge.test.js
@@ -18,6 +18,6 @@ describe("Astronaut", () => {
   test("should create a prototype method for determining a user's age based on the value 'Mercury' for the planet property", () => {
     const astronaut = new Astronaut(34, "Mercury"); 
     expect(astronaut.planet).toEqual("Mercury");
-    expect(astronaut.whereFrom()).toEqual(141.66);
+    expect(astronaut.whereFrom()).toEqual(141.67);
   });
 })
--- src/convertAge.js
+++ src/convertAge.js
@@ -8,10 +8,10 @@ export default class Astronaut {
     let presentAge = 0;
     if (this.planet === "Earth") {
       presentAge = this.age * 1
+      return parseFloat(presentAge.toFixed(2));
     } else if (this.planet === "Mercury") {
-      
+      presentAge = this.age / 0.24;
+      return parseFloat(presentAge.toFixed(2));
     }
-    return presentAge;
   }
-
 }
