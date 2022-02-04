055fc1b3fead8096a292eb229a999506c2d1fed7
Ryan Ashby <rjapsu160@gmail.com>
2022-02-04 08:35:07 -0800

    WIP - initial trouble building environment/built

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Euler',
+      title: 'Galactic Age',
       template: './src/index.html',
       inject: 'body'
     }),
