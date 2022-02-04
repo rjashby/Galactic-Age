edfe057e9ba1dae29ba0108b9ce8a57dc51f8380
Ryan Ashby <rjapsu160@gmail.com>
2022-02-04 14:37:57 -0800

    add form with two inputs to html and basic submit function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Astronaut from "./astronaut";
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    let age = parseInt($("input#age").val());
+    let planet = $("select#planet option:selected").val();
+    let astronaut = new Astronaut(age, planet);
+  })
+})
